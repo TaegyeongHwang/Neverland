c18a21d7f0b58c9a221c3bfc63f52a5fb0db6e2a
faraday <spacex.ocisly@gmail.com>
2022-04-05 23:40:44 +0900

    Update : destinationRegister -> AdminController

--- neverland/src/main/java/com/faraday/neverland/controller/AdminController.java
+++ neverland/src/main/java/com/faraday/neverland/controller/AdminController.java
@@ -1,13 +1,18 @@
 package com.faraday.neverland.controller;
 
 import com.faraday.neverland.domain.Travel;
+import com.faraday.neverland.form.DestinationRegisterForm;
+import com.faraday.neverland.service.DestinationService;
 import com.faraday.neverland.service.TravelService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -15,11 +20,41 @@ import java.util.List;
 @Slf4j
 public class AdminController {
 
+    private final DestinationService destinationService;
     private final TravelService travelService;
 
     /**
      * Admin
      */
+
+    /**
+     * 목적지
+     */
+    // 목적지 등록 페이지
+    @GetMapping("/admin/destinationRegister")
+    public String destinationRegisterPage(Model model) {
+        log.info("destinationRegisterPage()");
+
+        model.addAttribute("destinationRegisterForm", new DestinationRegisterForm());
+
+        return "/admin/destinationRegister";
+    }
+
+    // 목적지 등록하기
+    @PostMapping("/admin/registerProc")
+    public String destinationRegisterProc(@Valid DestinationRegisterForm form, BindingResult result) {
+        log.info("destinationRegisterProc");
+
+        if(result.hasErrors()) {
+
+            return "/admin/destinationRegister";
+        }
+
+        destinationService.destinationRegisterProc(form);
+
+        return "redirect:/";
+    }
+
     // 여행신청목록 페이지
     @GetMapping("/admin/travelRegisterList")
     public String travelRegisterList(Model model) {
